81ff0ef1944f62bb100a16f1c55adee43a3c6a24
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-10-27 22:27:58 +0000

    Adding countOnly exercise solution

--- countOnly.js
+++ countOnly.js
@@ -1,34 +1,39 @@
 const assertEqual = function(actual, expected) {
-    console.log(actual, expected);
-    (actual === expected) ? console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  };
+  console.log(actual, expected);
+  (actual === expected) ? console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
   
 
 const countOnly = (allItems, itemsToCount) => {
-    // Assign a variable called filteredObj to empty object
-    // iterate through the object keys and compare with the array
-    // Push all of the matching items into filteredObj only if they are true, the key will be the item and the value will start with 1;
+  // Assign a variable called filteredObj to empty object
+  const filteredObj = {};
+  // iterate through the array and check if any of the object keys match the array values
+  for (let key of allItems) {
     // If the item already exists in filteredObj, add 1 to the value of that key
-
-
-    // return filteredObj
-}
+    // If not, then add it to the filteredObj and assign 1 to its value
+    if (itemsToCount[key]) {
+      (filteredObj[key]) ? filteredObj[key] += 1 : filteredObj[key] = 1;
+    }
+  }
+  // return filteredObj
+  return filteredObj;
+};
 
 const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
   
-  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
   
-  assertEqual(result1["Jason"], 1);
-  assertEqual(result1["Karima"], undefined);
-  assertEqual(result1["Fang"], 2);
-  assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
