5b80ea02e71350d8c0dcd6b779802917eb070a54
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-10-27 13:39:13 +0000

    Submitting without exercise solution

--- without.js
+++ without.js
@@ -13,6 +13,16 @@ const assertEqualArrays = function(actual, expected) {
 
 
 const without = (source, itemsToRemove) => {
-  
-  
+  const filteredArr = [];
+
+  for (let val of source) {
+    if (!itemsToRemove.includes(val)) {
+      filteredArr.push(val);
+    }
+  }
+  return filteredArr;
 }
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(assertEqualArrays(without(['a', 'b', 'C', 'd', 'a'], [1, 'D', 'a', 'c']), ['b', 'C', 'd']));
