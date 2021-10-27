1429f239c23393acf8935ede4878913257aef3ea
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-10-27 23:40:58 +0000

    Adding findKeyByValue exercise solution

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  // console.log(actual, expected);
+  (actual === expected) ? console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (obj, val) => {
+  for (let key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+  
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
