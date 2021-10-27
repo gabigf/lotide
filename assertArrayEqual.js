6f0e8b0f7b874653a81e2c108abb21b489cbf1b3
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-10-27 13:46:22 +0000

    eslinted rest of files

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,8 +1,8 @@
 const eqArrays = (arr1, arr2) => {
-  for (i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1.length !== arr2.length || arr1[i] !== arr2[i]) {
       return false;
-    } 
+    }
   }
   return true;
 };
--- eqArrays.js
+++ eqArrays.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
-  for (i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i] || arr1[i] !== arr2[i]) {
       return false;
     }
