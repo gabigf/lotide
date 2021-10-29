a3d63089f53b0b88a427bccf7dbcad3760348cbb
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-10-29 03:34:28 +0000

    Adding Map 2 exercise solution

--- map.js
+++ map.js
@@ -12,7 +12,6 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 
-
 const map = function(arr, callback) {
   const results = [];
   
@@ -21,6 +20,9 @@ const map = function(arr, callback) {
   }
   return results;
 };
+
+
+// TEST CASES
 
 const words = ["ground", "control", "to", "major", "tom"];
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -32,3 +34,14 @@ console.log(assertArraysEqual(map(words, word => word.toUpperCase()), ['GROUND',
 console.log(assertArraysEqual(map(nums, num => num * 2), [2,4,6,8,10,12,14,16,18]));
 console.log(assertArraysEqual(map(truthOrFalse, bool => (!bool) ? true : true), [true, true, true, true, true]));
 
+// MAP 2 - Pythagorean theorem
+const input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+const result = input.map(val => Math.sqrt(Math.pow(val.x, 2) + Math.pow(val.y, 2)));
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
