ad8f32656780342c6cedb9ed685e4c9ed7587788
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-11-01 19:27:37 +0000

    Implemented mocha and chai

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,7 @@
-const eqArrays = (arr1, arr2) => {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1.length !== arr2.length || arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   (eqArrays(actual, expected)) ? console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-console.log(assertArraysEqual([1,2,4,5], [1,2,4,5]));
-console.log(assertArraysEqual([1,2,4,5], [1,2,4,'5']));
+module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,16 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  (eqArrays(actual, expected)) ? console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const middle = arr => {
   const midArray = [];
   
@@ -19,16 +6,11 @@ const middle = arr => {
   } else if (arr.length % 2 === 0) {
     midArray.push(arr[arr.length / 2 - 1], arr[arr.length / 2]);
   } else if (arr.length % 2 !== 0) {
-    midArray.push(arr[Math.ceil(arr.length / 2)]);
+    console.log(Math.floor(arr.length/2))
+    midArray.push(arr[Math.floor(arr.length / 2)]);
   } 
-
   return midArray;
 }
 
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle(['one', 'two', 'three', 'four']));
-console.log(assertArraysEqual(middle(['one', 'two', 'three', 'four']), ['two', 'three']));
-console.log(assertArraysEqual(middle(['one', 'two'], [])));
+
+module.exports = middle;
